b801d346e50a39b6e4a227d0908d93dc0181a4af
agmehta1 <agmehta@ucsd.edu>
2022-02-09 14:50:47 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -40,5 +40,6 @@ public class MarkdownParse {
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
+        
     }
 }
